77f19a65183e87e5729ab9f550db2699804ced4d
JadeVieTran <jadevtran@gmail.com>
2022-10-19 17:25:40 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 //Create a variable called isProduction that will be true if the environment is in production or not by checking the environment key in the configuration file(backend / config / index.js):
 
@@ -51,7 +52,44 @@ app.use(
 // backend/app.js
 const routes = require('./routes');
 
-// ...
-
 app.use(routes); // Connect all the routes
+
+//Resource Not Found Error-Handler
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+//Sequelize Error-Handler
+//The second error handler is for catching Sequelize errors and formatting them before sending the error response.
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+// Error Formatter Error-Handler
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
 module.exports = app;
