ede5d3437a5be9454c3e801e9214623ac1a63479
JadeVieTran <jadevtran@gmail.com>
2022-10-24 18:53:45 -0500

    Implement edit Review

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -30,4 +30,41 @@ router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
 
+// test route
+// // GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// });
+
+
+// // GET /api/restore-user
+// router.get(
+//     '/restore-user',
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+
+
+
+
 module.exports = router;
