c023afdb5b9b59a90417cd7de79aeb70d2bfb250
JadeVieTran <jadevtran@gmail.com>
2022-10-23 21:42:35 -0500

    Implement get all reviews of current user

---
+++ backend/routes/api/images.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { Op, sequelize, Sequelize } = require('sequelize');
+const { setTokenCookie, requireAuth, requireProperAuthorization } = require('../../utils/auth');
+const { Spot, SpotImage, Review, User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const router = express.Router();
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
+const bookingsRouter = require('./bookings.js');
+const reviewsRouter = require('./reviews.js');
+const imagesRouter = require('./images.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -16,46 +19,15 @@ router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
 
+router.use('/bookings', bookingsRouter);
+
+router.use('/reviews', reviewsRouter);
+
+router.use('/images', imagesRouter);
+
 // test route
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
-
-// test route
-// // GET /api/set-token-cookie
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     });
-//     setTokenCookie(res, user);
-//     return res.json({ user });
-// });
-
-
-// // GET /api/restore-user
-// router.get(
-//     '/restore-user',
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
-
-// // GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//     '/require-auth',
-//     requireAuth,
-//     (req, res) => {
-//         return res.json(req.user);
-//     }
-// );
-
-
-
-
 
 module.exports = router;
