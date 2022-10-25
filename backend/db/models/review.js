3c04d0f00a73b09943a55c59c22080e29b53d225
JadeVieTran <jadevtran@gmail.com>
2022-10-24 21:37:21 -0500

    Add constraint for validateRequestReview min-max

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -38,7 +38,11 @@ module.exports = (sequelize, DataTypes) => {
     },
     stars: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5
+      }
     }
   }, {
     sequelize,
