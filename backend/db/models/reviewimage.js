35cf065b28d95249cadc7571e03f51cebefd2c11
JadeVieTran <jadevtran@gmail.com>
2022-10-25 01:16:15 -0500

    Add scope for create image for Spot

--- backend/db/models/reviewimage.js
+++ backend/db/models/reviewimage.js
@@ -27,6 +27,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'ReviewImage',
+    defaultScope: {
+      attributes: {
+        exclude: ['createdAt', 'updatedAt', 'reviewId']
+      }
+    }
   });
   return ReviewImage;
 };
