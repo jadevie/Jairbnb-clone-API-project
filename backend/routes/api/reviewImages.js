814340a909c8640fb8d12dafade57f611bf71df8
JadeVieTran <jadevtran@gmail.com>
2022-10-26 16:36:42 -0500

    Implement delete reviewImage

--- backend/routes/api/reviewImages.js
+++ backend/routes/api/reviewImages.js
@@ -6,5 +6,31 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const userId = req.user.id;
+    const id = req.params.imageId;
+    const reviewImage = await ReviewImage.findByPk(id, {
+        raw: true,
+        nest: true,
+        include: {
+            model: Review,
+            attributes: ['userId']
+        }
+    });
+
+    if (reviewImage.Review.userId === userId) {
+        await ReviewImage.destroy({ where: { id } });
+        res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+        });
+    }
+    if (!reviewImage) {
+        res.status(404).json({
+            "message": "Review Image couldn't be found",
+            "statusCode": 404
+        });
+    }
+});
 
 module.exports = router;
