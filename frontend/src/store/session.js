c70026c2767bf14af36008d486657c02a8c7c2c9
JadeVieTran <jadevtran@gmail.com>
2022-11-11 01:02:07 -0600

    Set up login in session reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,41 @@
+import { csrfFetch } from './csrf';
+
+const LOGIN = "session/LOGIN";
+
+//* Action creater *//
+export const loginUser = (user) => {
+    return {
+        type: LOGIN,
+        payload: user
+    };
+};
+
+//* Thunk *//
+export const loginUserThunk = user => async (dispatch) => {
+    const response = await csrfFetch(`/api/session`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    });
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(loginUser(data));
+        return response;
+    }
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case LOGIN:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
